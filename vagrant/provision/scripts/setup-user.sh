8213df50fe9a0d1fdb2f68034a5ca19fe5b32599
Khosrow E <khosrow@khosrow.ca>
2016-12-29 18:51:05 -0500

    some code clean up

--- vagrant/provision/scripts/setup-user.sh
+++ vagrant/provision/scripts/setup-user.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 # Generate keys for vagrant user
-sudo mkdir -p ~vagrant/.ssh
-ssh-keygen -trsa -f ~vagrant/.ssh/id_rsa
-sudo chown vagrant.vagrant ~vagrant/.ssh/id_rsa
-sudo chown vagrant.vagrant ~vagramt/.ssh/id_rsa.pub
+mkdir -p ~/.ssh
+ssh-keygen -trsa -f ~/.ssh/id_rsa -q -N ""
+# sudo chown vagrant.vagrant ~/.ssh/id_rsa
+# sudo chown vagrant.vagrant ~/.ssh/id_rsa.pub
 
 # Setup passwordless access vagrant->vagrant
-echo -e "\n" >> ~vagrant/.ssh/authorized_keys
-sudo cat ~vagrant/.ssh/id_rsa.pub >> ~vagrant/.ssh/authorized_keys
+echo >> ~/.ssh/authorized_keys
+cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 
-#Fix perms
-sudo chmod 700 ~vagrant/.ssh
-sudo chmod 600 ~vagrant/.ssh/id_rsa
+# #Fix perms
+# sudo chmod 700 ~/.ssh
+# sudo chmod 600 ~/.ssh/id_rsa
