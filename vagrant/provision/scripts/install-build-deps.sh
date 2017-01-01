b1becf8f404ecdd539649c73788bd4c8f8f38dc9
Khosrow E <khosrow@khosrow.ca>
2017-01-01 16:06:40 -0500

    updating build depends

--- vagrant/provision/scripts/install-build-deps.sh
+++ vagrant/provision/scripts/install-build-deps.sh
@@ -6,8 +6,4 @@ sudo apt-get install -y unzip
 sudo apt-get install -y devscripts
 sudo apt-get install -y dpkg-dev
 sudo apt-get install -y debhelper
-sudo apt-get install -y python3-setuptools
 sudo apt-get install -y python-docutils
-sudo apt-get install -y git
-sudo apt-get install -y python3-pkg-resources
-sudo apt-get install -y ksh
