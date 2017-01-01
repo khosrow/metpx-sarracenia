de48c65e888e7d8d16ad22ab3a23543b71d06274
Khosrow E <khosrow@khosrow.ca>
2017-01-01 16:06:50 -0500

    updating test depends

--- vagrant/provision/scripts/install-deps.sh
+++ vagrant/provision/scripts/install-deps.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # get all the dependencies for rabbitmq-server and metpx-sarracenia
-##sudo add-apt-repository ppa:ssc-hpc-chp-spc/metpx -y
+sudo add-apt-repository ppa:ssc-hpc-chp-spc/metpx -y
 sudo apt-get update -q
 sudo apt-get install -y python3-setuptools
 sudo apt-get install -y rabbitmq-server
@@ -12,14 +12,17 @@ sudo apt-get install -y python3-pkg-resources
 #sudo apt-get install -y vsftpd
 sudo apt-get install -y ksh
 
-# if [ $1 = "trusty" ]; then
+if [ $1 = "trusty" ]; then
 	# sudo apt-get install -y python3-metpx-sarracenia
-	##sudo apt-get install -y python3-paramiko
-	##sudo apt-get install -y python3-amqplib
-	##sudo apt-get install -y python3-pyinotify
-	##sudo apt-get install -y python3-psutil
-	##sudo apt-get install -y python3-pip
-# fi
+	sudo apt-get install -y python3-pip
+	sudo apt-get install -y python3-amqplib
+	sudo apt-get install -y python3-appdirs
+	sudo apt-get install -y python3-watchdog
+	sudo apt-get install -y python3-netifaces
+	sudo apt-get install -y python3-humanize
+	sudo apt-get install -y python3-psutil
+	sudo apt-get install -y python3-paramiko
+fi
 
 if [ $1 = "precise" ]; then
 	sudo apt-get install python3-dev
